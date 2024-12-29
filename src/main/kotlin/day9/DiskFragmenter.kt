c88d61654b14432c3f00e35de7c9d71457e6bcfe
Carst Tankink <ctankink@bol.com>
2024-12-29 18:50:54 +0100

    AoC-day-9 Star 2.
    Quite some runtime on this one, probably because immutable lists are rather slow to manipulate at this size

--- src/main/kotlin/day9/DiskFragmenter.kt
+++ src/main/kotlin/day9/DiskFragmenter.kt
@@ -3,7 +3,7 @@ package day9
 import util.Solution
 
 class DiskFragmenter(fileName: String?) : Solution<List<Long?>, Long>(fileName) {
-    override fun parse(line: String): List<Long?>? {
+    override fun parse(line: String): List<Long?> {
         return line.windowed(2, 2, true)
             .withIndex()
             .flatMap { (idx, value) ->
@@ -21,7 +21,8 @@ class DiskFragmenter(fileName: String?) : Solution<List<Long?>, Long>(fileName) 
                     calculateChecksum(
                         blocks.drop(1),
                         emptyBlocks.drop(1),
-                        checksum + (b * (targetBlock ?: idx)))
+                        checksum + (b * (targetBlock ?: idx))
+                    )
                 } else {
                     calculateChecksum(
                         blocks.drop(1),
@@ -37,7 +38,91 @@ class DiskFragmenter(fileName: String?) : Solution<List<Long?>, Long>(fileName) 
         return calculateChecksum(disk.withIndex().reversed(), empty, 0)
     }
 
+    data class Block(
+        val start: Int,
+        val length: Int,
+        val fileId: Long?,
+    ) {
+        fun checksum(index: Int = start): Long {
+            return (0..<length).sumOf {
+                (index + it) * (fileId ?: 0)
+            }
+        }
+    }
+
+    private tailrec fun findBlocks(
+        list: List<IndexedValue<Long?>>,
+        currentBlock: Block?,
+        blocks: List<Block>
+    ): List<Block> {
+        return if (list.isEmpty()) {
+            blocks + (currentBlock?.let { listOf(it) } ?: emptyList())
+        } else {
+            val head = list.first()
+            val tail = list.drop(1)
+
+            when {
+                currentBlock == null -> {
+                    findBlocks(tail, Block(head.index, 1, head.value), blocks)
+                }
+
+                currentBlock.fileId == head.value -> {
+                    findBlocks(tail, currentBlock.copy(length = currentBlock.length + 1), blocks)
+                }
+
+                else -> {
+                    findBlocks(
+                        tail,
+                        Block(head.index, 1, head.value),
+                        blocks + currentBlock
+                    )
+                }
+            }
+
+        }
+    }
+
     override fun solve2(data: List<List<Long?>>): Long {
-        TODO("Not yet implemented")
+        val fileData = data.first().withIndex().toList()
+
+        val blocks = findBlocks(fileData, null, emptyList())
+        val emptyBlocks = blocks.filter { it.fileId == null }.withIndex().toList()
+
+        tailrec fun calculateChecksum(
+            blocks: List<Block>,
+            emptyBlocks: List<IndexedValue<Block>>,
+            checksum: Long
+        ): Long {
+            return if (blocks.isEmpty()) checksum else {
+                val block = blocks.first()
+                val tail = blocks.drop(1)
+
+                return if (block.fileId != null) {
+                    val emptyBlock = emptyBlocks.firstOrNull { it.value.length >= block.length }
+
+                    if (emptyBlock == null) {
+                        calculateChecksum(tail, emptyBlocks, checksum + block.checksum())
+                    } else {
+                        val newBlock = emptyBlock.value.copy(
+                            length = emptyBlock.value.length - block.length,
+                            start = emptyBlock.value.start + block.length
+                        )
+
+                        val newEmpties = emptyBlocks.map {
+                            if (it.index == emptyBlock.index) IndexedValue(
+                                it.index,
+                                newBlock
+                            ) else it
+                        }
+                        calculateChecksum(tail, newEmpties, checksum + block.checksum(emptyBlock.value.start))
+                    }
+
+                } else {
+                    calculateChecksum(tail, emptyBlocks.dropLast(1), checksum)
+                }
+            }
+        }
+
+        return calculateChecksum(blocks.reversed(), emptyBlocks, 0L)
     }
 }
--- src/test/kotlin/day9/DiskFragmenterTest.kt
+++ src/test/kotlin/day9/DiskFragmenterTest.kt
@@ -10,4 +10,9 @@ class DiskFragmenterTest {
     fun sample1star1() {
         assertEquals(1928, sample1.star1())
     }
+
+    @Test
+    fun sample1star2() {
+        assertEquals(2858, sample1.star2())
+    }
 }
